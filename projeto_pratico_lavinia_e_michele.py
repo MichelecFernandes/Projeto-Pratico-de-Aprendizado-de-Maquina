8fcd114c0e8ab495bf76e504302dc5a41dbef7d1
MichelecFernandes <michelecristinafr@gmail.com>
2025-05-15 12:21:58 -0300

    feat: criacao de menu de opcoes para ficar melhor apresentavel

--- projeto_pratico_lavinia_e_michele.py
+++ projeto_pratico_lavinia_e_michele.py
@@ -20,6 +20,49 @@
 
 #********************************************************
 
+import pandas as pd
+from youtube_transcript_api import YouTubeTranscriptApi
+import re
+from sentence_transformers import SentenceTransformer
+# pip install sentence-transformers
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+def gerarTrancricoes():
+    def obter_transcricao(video_id, idiomas=['pt', 'en']):
+        for idioma in idiomas:
+            try:
+                transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[idioma])
+                texto = " ".join([re.sub(r'[.*?\\]', '', entry['text']) for entry in transcript])
+                return " ".join(texto.split())  
+            except Exception:
+                continue 
+        return None 
+
+    videos_escolhidos = ['Y_befLlahys', '5Br-Z4Y3b-U', '9aI5v1cexWc', 'z2oF6Itzzyo', 'Uf-jBQB3aj0', 'i5Z9RKAB384', '5tNRcuzc4YU', 'PMs7KDyRo6Y', 'YZldtLXNfu8', 'E_OWENsOl5g',
+                        '2d0waLuCjzI', 'hPe7bA2s8XI', 'wbVPvVIepXw', 'IEPemH_lwLM', 'uAPy2sl-h7o', '1Yv28pSrmFw', 'dxX5HnH_tLA', '7XgfPGomGds', 'F_dUx77uwuc', 'posTc56basM',
+                        'vl4Z7F_NLxs', 'Nykwb2UNqvU', 'arJVNwjhmRY', 'suxIV1zNt9A', 'UlY2deWLyw0', '21mDekTZwsw', 'qVHPy7Np9rE', 'kEdaRdDkEiA', 'V3NMxTkR6n4', 'aw9lP3WAl2o',
+                        'tRcr4vtV-4o', 'CANYM34cIuQ', 'TUy6SC2MRig', '2fcxZTyaREk', 'eJjbzckuDPE', 'Tl9uyOqRdJ8', 'S01TrpEO148', 'uV0R0f1sy4Q', 'gj9R3nmB67Q', 'FWhFp4471k8',
+                        'WRlfwBof66s', 'pbb0jzXt_xA', 'MZgbd7bjCTk', 'jizAto4-ofs', 'y-_Ly5Tqggc', 'ZtgcWbcIWy4', 'eSrObbUHbTQ', 'hO_tjm9i32g', 'ZWYheuFOq_g', 'b-gniXXBXD0'   
+    ] 
+
+    dados = []
+    ids_de_videos_nao_transcritos = []
+
+    for vid in videos_escolhidos:
+        trans = obter_transcricao(vid)
+        if trans:
+            dados.append({"video_id": vid, "transcricao": trans})
+        else:
+            ids_de_videos_nao_transcritos.append(vid)
+
+    print("Transcricoes coletadas:", len(dados))
+    print("IDs nao transcritos:", ids_de_videos_nao_transcritos)
+
+    dataframe_dados = pd.DataFrame(dados)
+    dataframe_dados.to_csv("transcricoes.csv", index=False)
+
+
 # Passo 2: Representação dos Textos
 # Agora, precisamos transformar essas transcrições em um formato numérico que possa ser utilizado para agrupamento. Existem três métodos principais:
     # 1. Bag of Words (BoW): Conta quantas vezes cada palavra aparece em cada transcrição, ignorando a ordem.
@@ -31,7 +74,19 @@
     # • CountVectorizer para BoW
     # • SentenceTransformer para embeddings
 
-#********************************************************
+def transcricoesEmNumero():
+    # • CountVectorizer para BoW
+    dataframe_dados = pd.read_csv('transcricoes.csv')  # arquivo gerado no Passo 1
+
+    # Extrair lista de textos
+    textos = dataframe_dados['transcricao'].tolist()
+
+    # Inicializa o CountVectorizer
+    vetorizador = CountVectorizer(stop_words='english')  # pode usar stop_words='portuguese' se quiser
+    matriz_de_contagem_vetorizado = vetorizador.fit_transform(textos)
+
+    print(f"Matriz BoW: {matriz_de_contagem_vetorizado.shape} -> (n_transcricoes, n_palavras_no_vocabulario)")  # (n_transcricoes, n_palavras_no_vocabulario)
+
 
 # Passo 3: Aplicação do Algoritmo de Agrupamento
     # O agrupamento será feito com o algoritmo K-Means, que tenta separar os dados em grupos baseados em suas semelhanças.
@@ -71,43 +126,30 @@
 
     # 6. Código-fonte.
 
-import pandas as pd
-from youtube_transcript_api import YouTubeTranscriptApi
-import re
 
+def menu():
+    print("Projeto Prático de Apendizado de Máquina \n"
+    "PLN – Processamento de Linguagem Natural \n"
+    "Aprendizado Não-Supervisionado - Agrupamento")
+    print("Integrantes: Lavínia e Michele")
 
-def obter_transcricao(video_id, idiomas=['pt', 'en']):
-    for idioma in idiomas:
-        try:
-            transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[idioma])
-            texto = " ".join([re.sub(r'[.*?\\]', '', entry['text']) for entry in transcript])
-            return " ".join(texto.split())  
-        except Exception:
-            continue 
-    return None 
-
-videos_escolhidos = ['Y_befLlahys', '5Br-Z4Y3b-U', '9aI5v1cexWc', 'z2oF6Itzzyo', 'Uf-jBQB3aj0', 'i5Z9RKAB384', '5tNRcuzc4YU', 'PMs7KDyRo6Y', 'YZldtLXNfu8', 'E_OWENsOl5g',
-                     '2d0waLuCjzI', 'hPe7bA2s8XI', 'wbVPvVIepXw', 'IEPemH_lwLM', 'uAPy2sl-h7o', '1Yv28pSrmFw', 'dxX5HnH_tLA', '7XgfPGomGds', 'F_dUx77uwuc', 'posTc56basM',
-                     'vl4Z7F_NLxs', 'Nykwb2UNqvU', 'arJVNwjhmRY', 'suxIV1zNt9A', 'UlY2deWLyw0', '21mDekTZwsw', 'qVHPy7Np9rE', 'kEdaRdDkEiA', 'V3NMxTkR6n4', 'aw9lP3WAl2o',
-                     'tRcr4vtV-4o', 'CANYM34cIuQ', 'TUy6SC2MRig', '2fcxZTyaREk', 'eJjbzckuDPE', 'Tl9uyOqRdJ8', 'S01TrpEO148', 'uV0R0f1sy4Q', 'gj9R3nmB67Q', 'FWhFp4471k8',
-                     'WRlfwBof66s', 'pbb0jzXt_xA', 'MZgbd7bjCTk', 'jizAto4-ofs', 'y-_Ly5Tqggc', 'ZtgcWbcIWy4', 'eSrObbUHbTQ', 'hO_tjm9i32g', 'ZWYheuFOq_g', 'b-gniXXBXD0'   
-] 
-
-dados = []
-ids_de_videos_nao_transcritos = []
-
-for vid in videos_escolhidos:
-    trans = obter_transcricao(vid)
-    if trans:
-        dados.append({"video_id": vid, "transcricao": trans})
-    else:
-        ids_de_videos_nao_transcritos.append(vid)
-
-
-print("Transcricoes coletadas:", len(dados))
-print("IDs nao transcritos:", ids_de_videos_nao_transcritos)
-
-
-df = pd.DataFrame(dados)
-df.to_csv("transcricoes.csv", index=False)
-
+    opcao = -1
+    while opcao != 0:
+        print("1 - Gerar transcrições")
+        print("2 - Transformar as transcrições em numeros")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        opcao = int(input("Digite uma opção: "))
+ 
+        if opcao == 1:
+            gerarTrancricoes()
+        if opcao == 2:
+            transcricoesEmNumero()
+        elif opcao == 0:
+            print("Saindo.......")
+            break
+menu()
