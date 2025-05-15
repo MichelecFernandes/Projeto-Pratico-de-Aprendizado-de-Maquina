a0ac80e07221d4f8aca554a111e6b6ebe820752b
MichelecFernandes <michelecristinafr@gmail.com>
2025-05-15 11:04:26 -0300

    feat: escolha dos 50 videos para que seja possivel gerar as transcricoes

--- projeto_pratico_lavinia_e_michele.py
+++ projeto_pratico_lavinia_e_michele.py
@@ -85,14 +85,12 @@ def obter_transcricao(video_id, idiomas=['pt', 'en']):
             continue 
     return None 
 
-videos_escolhidos = ['OEg7DsudmMo', 'cY08Z_POkkk', 'yYMOqgu-x60', 'XGclzdIl1l',
-             'LOBEbDa8nV&t=5s', 'VcM4IKJTg70', 'cTRnZPGxrUg', 'Kxmeo',
-             'h8UTXijOTgA', 'o00cZUhw720', 'Oo_7yuGlvwg', 'ABAq7J5',
-             'WpewQxkjkkQ&t=21s', 'GhYbk_S7OKA', 'El-bpOP-AgI&t',
-             '6ujPifmXCTU', '3yr2DuszbMo', 'yQblgGXQ27o', 'h',
-             'HtzVP-cgazQ', '@k80jMCk6LU'] 
-
-
+videos_escolhidos = ['xucKeCMFeWM', '5Br-Z4Y3b-U', 'IZPdZTrqC9M', 'CcSIvhDlYWw', 'iltgpM4wHeY', 'i5Z9RKAB384', '5tNRcuzc4YU', 'PMs7KDyRo6Y', 'YZldtLXNfu8', 'E_OWENsOl5g',
+                     '2d0waLuCjzI', 'bbjCTfFHeYg', 'wbVPvVIepXw', 'IEPemH_lwLM', 'uAPy2sl-h7o', 'xeFgFj_ZbHs', '7B9deIA6JPY', 'CwzjlmBLfrQ', 'GJc4Ir78KdE', 'posTc56basM',
+                     'vl4Z7F_NLxs', 'gV691I3iPH8', 'PQmVfC3pD7w', 'suxIV1zNt9A', 'UlY2deWLyw0', '21mDekTZwsw', 'qVHPy7Np9rE', '7Iakje0cIQU', 'V3NMxTkR6n4', 'aw9lP3WAl2o',
+                     'tRcr4vtV-4o', 'CANYM34cIuQ', 'TUy6SC2MRig', '2fcxZTyaREk', 'eJjbzckuDPE', '2n5Fy7u3qlc', 'S01TrpEO148', 'uV0R0f1sy4Q', 'gj9R3nmB67Q', 'uRkrIqa-2wE',
+                     'WRlfwBof66s', 'pbb0jzXt_xA', 'MZgbd7bjCTk', 'Ztqaa-NWYQ8', 'y-_Ly5Tqggc', 'KEiPOKUq_Rw', 'eSrObbUHbTQ', 'rd1mCZVNnxE', 'ZWYheuFOq_g', 'b-gniXXBXD0'   
+] 
 
 dados = []
 ids_de_videos_nao_transcritos = []
