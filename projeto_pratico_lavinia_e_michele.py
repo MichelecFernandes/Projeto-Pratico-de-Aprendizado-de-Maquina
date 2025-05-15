4ee8eb729e8c61a629d0032ad3114ad10e614cf2
MichelecFernandes <michelecristinafr@gmail.com>
2025-05-15 11:32:38 -0300

    feat: seleacao de outros videos para maior informacoes de transcricao

--- projeto_pratico_lavinia_e_michele.py
+++ projeto_pratico_lavinia_e_michele.py
@@ -86,11 +86,11 @@ def obter_transcricao(video_id, idiomas=['pt', 'en']):
             continue 
     return None 
 
-videos_escolhidos = ['xucKeCMFeWM', '5Br-Z4Y3b-U', 'IZPdZTrqC9M', 'CcSIvhDlYWw', 'iltgpM4wHeY', 'i5Z9RKAB384', '5tNRcuzc4YU', 'PMs7KDyRo6Y', 'YZldtLXNfu8', 'E_OWENsOl5g',
-                     '2d0waLuCjzI', 'bbjCTfFHeYg', 'wbVPvVIepXw', 'IEPemH_lwLM', 'uAPy2sl-h7o', 'xeFgFj_ZbHs', '7B9deIA6JPY', 'CwzjlmBLfrQ', 'GJc4Ir78KdE', 'posTc56basM',
-                     'vl4Z7F_NLxs', 'gV691I3iPH8', 'PQmVfC3pD7w', 'suxIV1zNt9A', 'UlY2deWLyw0', '21mDekTZwsw', 'qVHPy7Np9rE', '7Iakje0cIQU', 'V3NMxTkR6n4', 'aw9lP3WAl2o',
-                     'tRcr4vtV-4o', 'CANYM34cIuQ', 'TUy6SC2MRig', '2fcxZTyaREk', 'eJjbzckuDPE', '2n5Fy7u3qlc', 'S01TrpEO148', 'uV0R0f1sy4Q', 'gj9R3nmB67Q', 'uRkrIqa-2wE',
-                     'WRlfwBof66s', 'pbb0jzXt_xA', 'MZgbd7bjCTk', 'Ztqaa-NWYQ8', 'y-_Ly5Tqggc', 'KEiPOKUq_Rw', 'eSrObbUHbTQ', 'rd1mCZVNnxE', 'ZWYheuFOq_g', 'b-gniXXBXD0'   
+videos_escolhidos = ['Y_befLlahys', '5Br-Z4Y3b-U', '9aI5v1cexWc', 'z2oF6Itzzyo', 'Uf-jBQB3aj0', 'i5Z9RKAB384', '5tNRcuzc4YU', 'PMs7KDyRo6Y', 'YZldtLXNfu8', 'E_OWENsOl5g',
+                     '2d0waLuCjzI', 'hPe7bA2s8XI', 'wbVPvVIepXw', 'IEPemH_lwLM', 'uAPy2sl-h7o', '1Yv28pSrmFw', 'dxX5HnH_tLA', '7XgfPGomGds', 'F_dUx77uwuc', 'posTc56basM',
+                     'vl4Z7F_NLxs', 'Nykwb2UNqvU', 'arJVNwjhmRY', 'suxIV1zNt9A', 'UlY2deWLyw0', '21mDekTZwsw', 'qVHPy7Np9rE', 'kEdaRdDkEiA', 'V3NMxTkR6n4', 'aw9lP3WAl2o',
+                     'tRcr4vtV-4o', 'CANYM34cIuQ', 'TUy6SC2MRig', '2fcxZTyaREk', 'eJjbzckuDPE', 'Tl9uyOqRdJ8', 'S01TrpEO148', 'uV0R0f1sy4Q', 'gj9R3nmB67Q', 'FWhFp4471k8',
+                     'WRlfwBof66s', 'pbb0jzXt_xA', 'MZgbd7bjCTk', 'jizAto4-ofs', 'y-_Ly5Tqggc', 'ZtgcWbcIWy4', 'eSrObbUHbTQ', 'hO_tjm9i32g', 'ZWYheuFOq_g', 'b-gniXXBXD0'   
 ] 
 
 dados = []
