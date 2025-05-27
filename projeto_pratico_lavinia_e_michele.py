35f9f7992b8fb283425aa6778fbbd27d0e306ec1
MichelecFernandes <michelecristinafr@gmail.com>
2025-05-27 12:02:20 -0300

    feat: geracao do arquivo csv referente a divisao de 10 grupos utilizando o algoritmo knn sobre o modelo bow

--- projeto_pratico_lavinia_e_michele.py
+++ projeto_pratico_lavinia_e_michele.py
@@ -146,6 +146,14 @@ def knn_bow(representacao='bow'):
         score = silhouette_score(X_dense, kmeans.labels_)
         print(f"Silhouette Score: {score}")
 
+    resultado = pd.DataFrame({
+    'video_id': video_ids,
+    'grupo': kmeans.labels_
+    })
+
+    resultado.to_csv('clusters_bow.csv', index=False)
+    print("CSV gerado: clusters_bow.csv")
+
     # Visualização PCA
     reduzir_matriz = PCA(n_components=2)
     X_reduced = reduzir_matriz.fit_transform(X.toarray() if representacao == 'bow' else X)
